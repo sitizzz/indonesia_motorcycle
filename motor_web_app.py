891e4342572f526ac76bc2c404bd8b48b0b0a75a
SitiZ <67043407+sitizzz@users.noreply.github.com>
2024-03-18 06:01:45 +0700

    Update motor_web_app.py

--- motor_web_app.py
+++ motor_web_app.py
@@ -58,6 +58,7 @@ def main():
     st.write("Hello, I am Siti Zubaidah, the creator of this web app!")
     st.write("This web application is my observation area to see whether streamlit able to run several machine learning models. It consists of three machine learning models for each brand. Namely: Honda, Suzuki and Yamaha. You can learn the code through my github [here](https://github.com/sitizzz)")
     st.write("If you consider to copy this web app. Kindly give credit to my name and hyperlink this web app.")
+    st.write("Learn data science [here](https://www.youtube.com/channel/UCeFBTC1HiIAR3rd11CuCM4A) (Bahasa Indonesia Only)")
     
     #getting the input data from the user
     Merek = st.selectbox('Choose brand: ', ['Honda', 'Suzuki', 'Yamaha'])
